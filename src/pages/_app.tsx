40f80886db6245ddaf4d9450ca1f624b418cf4b7
0xUkhezo <ukhezo.web3@gmail.com>
2023-11-13 15:26:30 +0100

    feat(app): add wagmi conmfig to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,39 @@
+// Next
 import type { AppProps } from "next/app";
+// Wagmi
+import { WagmiConfig, configureChains, createConfig } from "wagmi";
+import { mainnet, arbitrum, polygonMumbai } from "wagmi/chains";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 
 import "@/styles/globals.css";
 
+const { publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, arbitrum, polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API as string }),
+    publicProvider(),
+  ]
+);
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+  connectors: [
+    new InjectedConnector({
+      options: {
+        shimDisconnect: false,
+      },
+    }),
+  ],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={config}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
