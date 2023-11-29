2992f79088ad865783e273dedfa79e846eb31077
0xUkhezo <ukhezo.web3@gmail.com>
2023-11-29 18:16:00 +0100

    feat(components): about section overview fund

---
+++ src/components/Cards/AboutFundCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type AboutFundCardProps = {
+  description: string;
+};
+
+export default function AboutFundCard({ description }: AboutFundCardProps) {
+  return (
+    <main className="max-w-[792px] pl-[40px] pr-[104px] py-[40px] bg-white rounded-xl text-xl font-bold shadow-xl">
+      {description}
+    </main>
+  );
+}
